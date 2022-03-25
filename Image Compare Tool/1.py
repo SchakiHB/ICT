9aa552ae0d7d0064d1cdfa950100f15f6db0fa66
SchakiHB <norman.hendrich@yahoo.de>
2022-03-25 15:42:04 +0100

    frame added

--- Image Compare Tool/1.py
+++ Image Compare Tool/1.py
@@ -28,25 +28,70 @@ def create_img2():
     img2orig = cv2.cvtColor(img2orig, cv2.COLOR_BGR2RGB)
 
 
-class App(tk.Frame):
+class ScrollbarFrame(tk.Frame):
+    """
+    Extends class tk.Frame to support a scrollable Frame
+    This class is independent from the widgets to be scrolled and
+    can be used to replace a standard tk.Frame
+    """
+    def __init__(self, parent, **kwargs):
+        tk.Frame.__init__(self, parent, **kwargs)
+
+        # The Scrollbar, layout to the right
+        vsb = tk.Scrollbar(self, orient="vertical")
+        vsb.pack(side="right", fill="y")
+
+        # The Canvas which supports the Scrollbar Interface, layout to the left
+        self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+        # Bind the Scrollbar to the self.canvas Scrollbar Interface
+        self.canvas.configure(yscrollcommand=vsb.set)
+        vsb.configure(command=self.canvas.yview)
+
+        # The Frame to be scrolled, layout into the canvas
+        # All widgets to be scrolled have to use this Frame as parent
+        self.scrolled_frame = tk.Frame(self.canvas, background=self.canvas.cget('bg'))
+        self.canvas.create_window((4, 4), window=self.scrolled_frame, anchor="nw")
+
+        # Configures the scrollregion of the Canvas dynamically
+        self.scrolled_frame.bind("<Configure>", self.on_configure)
+
+    def on_configure(self, event):
+        """Set the scroll region to encompass the scrolled frame"""
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+
+class App(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         global img1orig,img2orig
 
-        tk.Frame.__init__(self, *args, **kwargs)
-        btn1 = tk.Button(text='Load image1', command=create_img1)
-        btn2 = tk.Button(text='Load image2', command=create_img2)
-        btn3 = tk.Button(root, text="show combined", command=lambda *args: App.show_combined(self,img1orig, img2orig))
-        btnup = tk.Button(text='up', command=lambda *args: App.increase_offset_y(self,img1orig, img2orig))
-        btndown = tk.Button(text='down', command=lambda *args: App.decrease_offset_y(self,img1orig, img2orig))
-        btnright = tk.Button(text='right', command=lambda *args: App.increase_offset_x(self,img1orig, img2orig))
-        btnleft = tk.Button(text='left', command=lambda *args: App.decrease_offset_x(self,img1orig, img2orig))
-        btnreset = tk.Button(text='reset', command=lambda *args: App.reset_offsets(self,img1orig, img2orig))
-        btnstep1 = tk.Button(text='Stepsize 1', command=lambda *args: App.change_stepsize(self,1))
-        btnstep5 = tk.Button(text='Stepsize 5', command=lambda *args: App.change_stepsize(self,5))
-        btnstep10 = tk.Button(text='Stepsize 10', command=lambda *args: App.change_stepsize(self,10))
-        btnzoomin = tk.Button(text='Zoom in', command=lambda *args: App.zoom_in(self,img1orig, img2orig))
-        btnzoomout = tk.Button(text='Zoom out', command=lambda *args: App.zoom_out(self,img1orig, img2orig))
+        # tk.Frame.__init__(self, *args, **kwargs)
+        super().__init__()
+
+        sbf = ScrollbarFrame(self)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        sbf.grid(row=0, column=0, sticky='nsew')
+        # sbf.pack(side="top", fill="both", expand=True)
+
+        # Some data, layout into the sbf.scrolled_frame
+        frame = sbf.scrolled_frame
+
+        btn1 = tk.Button(frame, text='Load image1', command=create_img1)
+        btn2 = tk.Button(frame, text='Load image2', command=create_img2)
+        btn3 = tk.Button(frame, text="show combined", command=lambda *args: App.show_combined(self,img1orig, img2orig,frame))
+        btnup = tk.Button(frame, text='up', command=lambda *args: App.increase_offset_y(self,img1orig, img2orig, frame))
+        btndown = tk.Button(frame, text='down', command=lambda *args: App.decrease_offset_y(self,img1orig, img2orig, frame))
+        btnright = tk.Button(frame, text='right', command=lambda *args: App.increase_offset_x(self,img1orig, img2orig, frame))
+        btnleft = tk.Button(frame, text='left', command=lambda *args: App.decrease_offset_x(self,img1orig, img2orig, frame))
+        btnreset = tk.Button(frame, text='reset', command=lambda *args: App.reset_offsets(self,img1orig, img2orig, frame))
+        btnstep1 = tk.Button(frame, text='Stepsize 1', command=lambda *args: App.change_stepsize(self,1))
+        btnstep5 = tk.Button(frame, text='Stepsize 5', command=lambda *args: App.change_stepsize(self,5))
+        btnstep10 = tk.Button(frame, text='Stepsize 10', command=lambda *args: App.change_stepsize(self,10))
+        btnzoomin = tk.Button(frame, text='Zoom in', command=lambda *args: App.zoom_in(self,img1orig, img2orig, frame))
+        btnzoomout = tk.Button(frame, text='Zoom out', command=lambda *args: App.zoom_out(self,img1orig, img2orig, frame))
 
         btn1.config(width=20, height=2)
         btn2.config(width=20, height=2)
@@ -76,39 +121,39 @@ class App(tk.Frame):
         btnzoomin.grid(row=8, column=1)
         btnzoomout.grid(row=8, column=3)
 
-    def increase_offset_x(self,img1, img2):
+    def increase_offset_x(self,img1, img2, frame):
         global manual_offset, offset_x, stepsize
         manual_offset = True
         offset_x = offset_x + stepsize
         img1 = img1.copy()
         img2 = img2.copy()
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
-    def decrease_offset_x(self,img1, img2):
+    def decrease_offset_x(self,img1, img2, frame):
         global manual_offset, offset_x, stepsize
         manual_offset = True
         offset_x = offset_x - stepsize
         img1 = img1.copy()
         img2 = img2.copy()
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
-    def increase_offset_y(self,img1, img2):
+    def increase_offset_y(self,img1, img2, frame):
         global manual_offset, offset_y, stepsize
         manual_offset = True
         offset_y = offset_y + stepsize
         img1 = img1.copy()
         img2 = img2.copy()
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
-    def decrease_offset_y(self,img1, img2):
+    def decrease_offset_y(self,img1, img2, frame):
         global manual_offset, offset_y, stepsize
         manual_offset = True
         offset_y = offset_y - stepsize
         img1 = img1.copy()
         img2 = img2.copy()
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
-    def reset_offsets(self,img1, img2):
+    def reset_offsets(self,img1, img2, frame):
         global manual_offset
         manual_offset = False
         global offset_y
@@ -117,23 +162,23 @@ class App(tk.Frame):
         offset_x = 0
         img1 = img1.copy()
         img2 = img2.copy()
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
     def change_stepsize(self,size):
         global stepsize
         stepsize = size
 
-    def zoom_in(self,img1, img2):
+    def zoom_in(self,img1, img2, frame):
         global zoom
         zoom = zoom * 1.1
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
-    def zoom_out(self,img1, img2):
+    def zoom_out(self,img1, img2, frame):
         global zoom
         zoom = zoom / 1.1
-        App.show_combined(self,img1, img2)
+        App.show_combined(self,img1, img2, frame)
 
-    def resize_and_combine_images(self,image1, image2):
+    def resize_and_combine_images(self,image1, image2, frame):
         global manual_offset
         global offset_x, offset_y
 
@@ -184,9 +229,9 @@ class App(tk.Frame):
 
         return img1, img2, combined
 
-    def calc_differences(self,image1, image2):
+    def calc_differences(self,image1, image2, frame):
 
-        resizedimage1, resizedimage2, combined = App.resize_and_combine_images(self,image1, image2)
+        resizedimage1, resizedimage2, combined = App.resize_and_combine_images(self,image1, image2, frame)
 
         img1 = resizedimage1.copy()
         img2 = resizedimage2.copy()
@@ -220,7 +265,7 @@ class App(tk.Frame):
 
         return img1, img2, combined, thresh, result1, result2
 
-    def show_combined(self,img1, img2):
+    def show_combined(self,img1, img2, frame):
         global photo1, photo2, photo3, photo4, photo5, photo6, zoom
         dimx = img1.shape[0]
         dimy = img1.shape[0]
@@ -229,7 +274,7 @@ class App(tk.Frame):
 
         newdim = (dimx, dimy)
 
-        img1, img2, combined, thresh, result1, result2 = App.calc_differences(self,img1, img2)
+        img1, img2, combined, thresh, result1, result2 = App.calc_differences(self,img1, img2, frame)
         img1 = cv2.resize(img1, newdim)
         img2 = cv2.resize(img2, newdim)
         combined = cv2.resize(combined, newdim)
@@ -241,38 +286,41 @@ class App(tk.Frame):
         width = 250
         height = 250
         photo1 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(img1))
-        canvas1 = tk.Canvas(root, width=width, height=height)
+        canvas1 = tk.Canvas(frame, width=width, height=height)
         canvas1.create_image(0, 0, image=photo1, anchor=tk.NW)
         canvas1.grid(row=0, column=4, rowspan=3, columnspan=3)
         photo2 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(img2))
-        canvas2 = tk.Canvas(root, width=width, height=height)
+        canvas2 = tk.Canvas(frame, width=width, height=height)
         canvas2.create_image(0, 0, image=photo2, anchor=tk.NW)
         canvas2.grid(row=0, column=7, rowspan=3, columnspan=3)
         photo3 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(combined))
-        canvas3 = tk.Canvas(root, width=width, height=height)
+        canvas3 = tk.Canvas(frame, width=width, height=height)
         canvas3.create_image(0, 0, image=photo3, anchor=tk.NW)
         canvas3.grid(row=4, column=4, rowspan=3, columnspan=3)
         photo4 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(thresh))
-        canvas4 = tk.Canvas(root, width=width, height=height)
+        canvas4 = tk.Canvas(frame, width=width, height=height)
         canvas4.create_image(0, 0, image=photo4, anchor=tk.NW)
         canvas4.grid(row=4, column=7, rowspan=3, columnspan=3)
         photo5 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(result1))
-        canvas5 = tk.Canvas(root, width=width, height=height)
+        canvas5 = tk.Canvas(frame, width=width, height=height)
         canvas5.create_image(0, 0, image=photo5, anchor=tk.NW)
         canvas5.grid(row=7, column=4, rowspan=3, columnspan=3)
         photo6 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(result2))
-        canvas6 = tk.Canvas(root, width=width, height=height)
+        canvas6 = tk.Canvas(frame, width=width, height=height)
         canvas6.create_image(0, 0, image=photo6, anchor=tk.NW)
         canvas6.grid(row=7, column=7, rowspan=3, columnspan=3)
 
 
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     root.geometry("1200x800+300+150")
+#     root.title("Image Comparing Tool")
+#     root.resizable(width=True, height=True)
+#     app = App(root)
+#     root.mainloop()
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("1200x800+300+150")
-    root.title("Image Comparing Tool")
-    root.resizable(width=True, height=True)
-    app = App(root)
-    root.mainloop()
+    App().mainloop()
 
 
 
